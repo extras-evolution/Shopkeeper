312ab1af40b2536286b24217479112eb0bab1d5c
MrSwed <webmaster@sdcollection.com>
2015-03-31 11:51:02 +0300

    Обновление phx modifiers

--- assets/plugins/phx/modifiers/num_format.phx.php
+++ assets/plugins/phx/modifiers/num_format.phx.php
@@ -1,5 +1,14 @@
 <?php
 
-return number_format($output,(floor($output) == $output ? 0 : 2),'.',' ');
+/*
+    * description: format a numeric value
+    * usage: [+variable:num_format=`decimals|dec_point|thousands_sep`+]
+    * 
+*/
+list($decimals,$dec_point,$thousands_sep) = explode("|",$options,3);
+if (!$decimals) $decimals = 2;
+if (!$dec_point) $dec_point = ".";
+
+return number_format($output,$decimals,$dec_point,$thousands_sep);
 
 ?>
