a71fa081072cd045e88608a166f297ee48c8b03b
MrSwed <webmaster@sdcollection.com>
2016-12-15 15:27:12 +0300

    phx update

--- assets/plugins/phx/modifiers/num_format.phx.php
+++ assets/plugins/phx/modifiers/num_format.phx.php
@@ -9,6 +9,6 @@ list($decimals,$dec_point,$thousands_sep) = explode("|",$options,3);
 if (!$decimals) $decimals = 2;
 if (!$dec_point) $dec_point = ".";
 
-return number_format($output,$decimals,$dec_point,$thousands_sep);
+return number_format((float)$output,$decimals,$dec_point,$thousands_sep);
 
 ?>
